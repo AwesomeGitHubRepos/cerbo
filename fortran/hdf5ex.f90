7adc80f6c732dc48ec0610968a6c47d7f8e1ec2c
mark carter <devnull@markcarter.me.uk>
2014-12-23 15:27:46 +0000

    modified:   hdf5ex.f90 modified:   hdf5ex.txt

--- fortran/hdf5ex.f90
+++ fortran/hdf5ex.f90
@@ -5,22 +5,26 @@
 
 program hdf5ex
 USE HDF5
+implicit none
 
 character:: filename = "/home/mcarter/.fortran/itrk_l.h5"
-character(len=100):: h5file
+character(len=10):: symbol
 integer(HID_T) :: file_id, dset_id, dspace_id, dataspace
 integer*8:: npoints
 integer:: error
-double precision:: buff(10000), rsi(10000)
+double precision, dimension(10000):: closing, rsi14, sma20, sma50, sma200, vol
 !sd_id = sfstart(file_name, DFACC_READ)
-integer(Hsize_t), dimension(1:2):: dims = (/10000, 1 /)
+!integer(Hsize_t), dimension(1:2):: dims = (/10000, 1 /)
+integer::dstamp, i
+logical::aug_h5 ! set to true if you want to update the HDF5 file
+integer(Hsize_t), dimension(1):: dims
 
-
-namelist /config/ h5file
+namelist /config/ symbol, aug_h5
 
 open(unit=10, file = "hdf5ex.txt")
 read(10, nml = config)
-print *, "Processing file: ", h5file
+print *, "Processing symbol: ", symbol
+print *, "Writing HDF5 file: ", aug_h5
 close(10)
 !stop 0
 
@@ -29,7 +33,7 @@ if(error.eq.-1)stop 0
 call chdir("/home/mcarter/.fortran")
 !CALL h5fopen_f (filename, H5F_ACC_RDWR_F, file_id, error)
 !CALL h5fopen_f ("itrk_l.h5", H5F_ACC_RDONLY_F, file_id, error)
-CALL h5fopen_f (h5file, H5F_ACC_RDWR_F, file_id, error)
+CALL h5fopen_f (trim(symbol) // ".h5", H5F_ACC_RDWR_F, file_id, error)
 
 if(error.eq.-1)stop 1
 CALL h5dopen_f(file_id, "closing", dset_id, error)
@@ -38,35 +42,69 @@ CALL h5dget_space_f(dset_id, dspace_id, error)
 if(error.eq.01)stop 3
 call h5sget_simple_extent_npoints_f(dspace_id, npoints, error)
 if(error.eq.-1)stop 4
-print *, "***********here"
 print *, "Contains the following number of points:", npoints
 if(npoints>10000)stop 5
-CALL h5dread_f(dset_id, H5T_NATIVE_DOUBLE, buff, dims, error)
+CALL h5dread_f(dset_id, H5T_NATIVE_DOUBLE, closing, dims, error)
 if(error.eq.-1)stop 6
 !print *, "values are:", buff(1:npoints)
 CALL h5dclose_f(dset_id, error)
 if(error.eq.-1)stop 7
 
-! now let's get ambitious and write a dataset
-call calc_rsi(buff, npoints, 14, rsi)
-dims(1) = npoints
-buff = buff + 0.1
-!call h5fcreate_simple_f(1, dims, dspace_id, error)
-call h5dcreate_f(file_id, "rsi14", H5T_NATIVE_DOUBLE, dspace_id, dset_id, error)
-if(error.eq.01)stop 11
-call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, rsi, dims, error)
-if(error.eq.-1)stop 10
-call h5dclose_f(dset_id, error)
-if(error.eq.-1)stop 12
+call calc_rsi(closing, npoints, 14, rsi14)
+call calc_sma(closing, npoints, 20, sma20)
+call calc_sma(closing, npoints, 50, sma50)
+call calc_sma(closing, npoints, 200, sma200)
+
+if(aug_h5)then
+   call write_h5_darray(file_id, dspace_id, rsi14, npoints, "rsi14")
+   call write_h5_darray(file_id, dspace_id, sma20, npoints, "sma20")
+   call write_h5_darray(file_id, dspace_id, sma50, npoints, "sma50")
+   call write_h5_darray(file_id, dspace_id, sma200, npoints, "sma200")
+endif
 
 CALL h5fclose_f(file_id, error)
 if(error.eq.-1)stop 8
 CALL h5close_f(error)
 if(error.eq.-1)stop 9
 
-print *, "it compiles"
+!dump data
+open(unit =11, file = trim(symbol) // ".dat")
+write(11,*) "#idx dstamp closing vol rsi14 sma20 sma50 sma200"
+dstamp = 666 ! TODO fix
+vol = 666 ! TODO fix
+do i = 1,npoints
+   write(11,"(I0,I10,6F8.3)") i, dstamp, closing(i), vol(i), rsi14(i), sma20(i), sma50(i), sma200(i)
+enddo
+close(11)
+
+print *, "Finished"
 
 end program 
+
+
+subroutine write_h5_darray(file_id, dspace_id, arr, npoints, name)
+  use HDF5
+  integer(HID_T), intent(in) :: file_id
+  integer(HID_T), intent(in) :: dspace_id
+  integer*8, intent(in):: npoints
+  double precision, intent(in), dimension(npoints):: arr
+  character(len=*), intent(in):: name !name of the dataset
+
+
+  integer(HID_T):: dset_id
+  integer(Hsize_t), dimension(1):: dims
+  integer:: error
+
+  dims(1) = npoints
+
+  call h5dcreate_f(file_id, name, H5T_NATIVE_DOUBLE, dspace_id, dset_id, error)
+  if(error.eq.01)stop 501
+  call h5dwrite_f(dset_id, H5T_NATIVE_DOUBLE, arr, dims, error)
+  if(error.eq.-1)stop 502
+  call h5dclose_f(dset_id, error)
+  if(error.eq.-1)stop 503
+end subroutine write_h5_darray
+
 
 subroutine calc_rsi(prices, npoints, n, rsi)
   ! appears to be correct
@@ -102,3 +140,20 @@ subroutine calc_rsi(prices, npoints, n, rsi)
      rsi(i) = 100.0 * rs/(1.0+rs)
   enddo
 end subroutine calc_rsi
+
+
+subroutine calc_sma(prices, npoints, n, sma)
+  !looks good
+  ! calculate a simple moving average
+  integer*8, intent(in) :: npoints
+  double precision, intent(in), dimension(npoints):: prices
+  integer, intent(in) :: n
+  double precision, intent(out), dimension(npoints) ::  sma
+
+  integer*8::  i, i0
+
+  do i= 1, npoints
+     i0 = max(1, 1+ i -n) ! cope with beginning points
+     sma(i) = sum(prices(i0:i))/dble(i - i0+1)
+  end do
+end subroutine
