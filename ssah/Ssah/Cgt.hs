3d5899b306641ec5b228243811635713fe3d973c
mark carter <devnull@markcarter.me.uk>
2015-05-27 18:51:59 +0100

    modified:   ssah/Ssah/Cgt.hs

--- ssah/Ssah/Cgt.hs
+++ ssah/Ssah/Cgt.hs
@@ -2,9 +2,11 @@
 module Ssah.Cgt where
 
 import Data.List
+import Data.List.Split
 import Data.Maybe
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Text.Printf
 
 import Ssah.Comm  
 import Ssah.Etran
@@ -23,7 +25,19 @@ commSymSold es =
     cs2 = Set.fromList cs1 -- to remove dupes
     cs3 = Set.toList cs2
     cs4 = sort cs3
-          
+
+
+mkRow e =
+  intercalate "\t" [bs, dstamp, etSym e, shareStr, priceStr, "0.00", "0.00"]
+  where
+    bs = if etIsBuy e then "B" else "S"
+    [y, m, d] = splitOn "-" $ etDstamp e
+    dstamp = intercalate "/" [d, m, y]
+    shares = abs $ etQty e
+    shareStr = printf "%0.4f" shares
+    price = abs $  (unPennies $ etAmount e) / shares
+    priceStr = printf "%0.5f" price
+    
 -- | create the CGT spreadsheet
 mkCgt etrans =
   x
@@ -31,9 +45,17 @@ mkCgt etrans =
     es1 = filter (isJust . etDerived) etrans
     es2 = feopn "tdi" (/=) es1 -- completely ignore the ISA
     cs = commSymSold es2
-    
-    x = cs
+
+    -- find those etrans which have comms that have sales
+    es3 = filter (\e -> elem (commSym $ etCommA e) cs) es2
+
+    es4 = sortOnMc (\e -> (etSym e, etDstamp e)) es3
+    eRows = map mkRow es4
+    x = eRows
 
 createCgtReport etrans = do
-  putStrLn "FIXME NOW createCgtReport "
-  putStrLn $ show $ mkCgt etrans
+  --let hdr = "B/S,Date,Company,Share,Price,Charges,Tax"
+  let trans = mkCgt etrans
+  --let rows = [hdr] ++ trans
+  writeFile "/home/mcarter/.ssa/cgt.txt" $ intercalate "\n" trans
+  -- putStrLn $ show $ mkCgt etrans
