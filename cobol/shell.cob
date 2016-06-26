8fa50f85a574f6aa08b5146c50eedbda4697fe7b
Mark Carter <alt.mcarter@gmail.com>
2016-06-26 13:26:08 +0100

    Improved shell.cob

--- cobol/shell.cob
+++ cobol/shell.cob
@@ -1,16 +1,41 @@
 001000 IDENTIFICATION DIVISION.
-001010 PROGRAM-ID. ???.
+      * 2016-06-26 created by mcarter
+      * shell reads shell.cob and echos it to output
+001010 PROGRAM-ID. shell.
 
 002000 ENVIRONMENT DIVISION.
 002010 INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           select fd-in assign to ws-fd-in-name
+                   organisation is line sequential.
 
 003000 DATA DIVISION.
 003010 FILE SECTION.
+       FD fd-in.
+       01 inline pic x(80).
+
 003020 WORKING-STORAGE SECTION.
+       01 ws-fd-in-name pic x(50).
+
 003030 LOCAL-STORAGE SECTION.
 003040 LINKAGE SECTION.
 
 004000 PROCEDURE DIVISION.
        program-begin.
+           move "shell.cob" to ws-fd-in-name.
+           display "=== ECHOING FILE ==="
+           open input fd-in.           
+
+           perform forever 
+           read fd-in
+                   at end exit perform
+                   not at end display inline
+           end-read
+           end-perform
+
+
+           display "=== FINISHED ==="
+           close fd-in.
+
        program-done.
            stop run.
