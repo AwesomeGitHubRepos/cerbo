915821795b5516537c4b3981a8d2fd5659bb1bba
Mark Carter <alt.mcarter@gmail.com>
2016-10-25 21:11:43 +0100

    autogen now no longer assumes c++14

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 autoreconf -iv
-./configure --prefix=$HOME/.local CXXFLAGS='-std=gnu++14'
+#./configure --prefix=$HOME/.local CXXFLAGS='-std=gnu++14'
+./configure --prefix=$HOME/.local
