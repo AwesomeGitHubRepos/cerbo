b5ab42c471a0721a6b8be266267e27de548c1961
Mark Carter <alt.mcarter@gmail.com>
2016-10-31 15:23:14 +0000

    Added common library:supo

---
+++ supo/supo.cc
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <iostream>
+
+#include "supo.hpp"
+
+namespace supo {
+void ssystem(const std::string& command, bool report_problem_to_stderr)
+{
+	int ret = system(command.c_str());
+	if(ret!=0 && report_problem_to_stderr)
+		std::cerr 
+			<< "WARN: suppo::ssystem() non-zero exit on command: <"
+			<< command
+			<< ">"
+			<< std::endl;
+}
+
+
+
+
+} // namespace supo
---
+++ supo/supo.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace supo {
+
+void ssystem(const std::string& command, bool report_problem_to_stderr);
+
+}
