b2cd47a4839fdeb70e5b06e852767268676de96f
Mark Carter <alt.mcarter@gmail.com>
2016-11-01 20:36:59 +0000

    minor typo fix

--- supo/supo.cc
+++ supo/supo.cc
@@ -9,7 +9,7 @@ void ssystem(const std::string& command, bool report_problem_to_stderr)
 	int ret = system(command.c_str());
 	if(ret!=0 && report_problem_to_stderr)
 		std::cerr 
-			<< "WARN: suppo::ssystem() non-zero exit on command: <"
+			<< "WARN: supo::ssystem() non-zero exit on command: <"
 			<< command
 			<< ">"
 			<< std::endl;
