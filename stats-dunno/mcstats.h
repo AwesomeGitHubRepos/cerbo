ac9d4eba098ea4488bb5a09756592f86719880dd
Mark Carter <alt.mcarter@gmail.com>
2016-11-19 12:00:28 +0000

    Build fixes (again!)

--- stats-dunno/mcstats.h
+++
@@ -1,9 +0,0 @@
-#ifndef MCSTATS_H
-#define MCSTATS_H
-
-void exp_fit_y(double *y, int n);
-void least_squares(double *x, double *y, int n, double *a , double *b);
-void pstat(char *name, double value);
-double sstdev(double *x, int n);
-
-#endif // MCSTATS_H
