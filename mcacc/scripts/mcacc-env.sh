efbb5f33bffe246d99154c6a08301f30cecf22bc
Mark Carter <alt.mcarter@gmail.com>
2016-12-06 15:50:18 +0000

    modified:   mcacc-env.sh

--- mcacc/scripts/mcacc-env.sh
+++ mcacc/scripts/mcacc-env.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
-root=`mcacc --root`
+#root=`mcacc --root`
+root=$HOME/.mcacc
 arc=$root/arc
 w=$root/work
 s1=$w/s1
 s2=$w/s2
 s3=$w/s3
+#echo "root=$root, s1=$s1"
+
 
 export root arc w s1 s2 s3
 #cd $w
