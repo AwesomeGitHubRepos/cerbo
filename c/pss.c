6aa9a9cc3617d5e0ecdebc76730d69815050f6a8
Mark Carter <alt.mcarter@gmail.com>
2017-07-03 14:50:43 +0100

    Adjustment to pss.c

--- c/pss.c
+++ c/pss.c
@@ -5,9 +5,10 @@
 #include <proc/readproc.h>
 
 int main(int argc, char** argv) {
-  PROCTAB* proc = openproc(PROC_FILLMEM | PROC_FILLSTAT | PROC_FILLSTATUS);
   proc_t proc_info;
   memset(&proc_info, 0, sizeof(proc_info));
+
+  PROCTAB* proc = openproc(PROC_FILLMEM | PROC_FILLSTAT | PROC_FILLSTATUS);
   while (readproc(proc, &proc_info) != NULL) {
     printf("%20s:\t%5ld\t%5lld\t%5lld\n",
            proc_info.cmd, proc_info.resident,
