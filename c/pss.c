4f4fe94148f6565bf44948ac230b45c0515fce10
Mark Carter <alt.mcarter@gmail.com>
2017-08-29 13:45:10 +0100

    Modified pss.c
    As per suggestion at:
    https://github.com/blippy/cerbo/issues/1

--- c/pss.c
+++ c/pss.c
@@ -8,14 +8,13 @@
 #include <proc/readproc.h>
 
 int main(int argc, char** argv) {
-  proc_t proc_info;
-  memset(&proc_info, 0, sizeof(proc_info));
-
-  PROCTAB* proc = openproc(PROC_FILLMEM | PROC_FILLSTAT | PROC_FILLSTATUS);
-  while (readproc(proc, &proc_info) != NULL) {
-    printf("%20s:\t%5ld\t%5lld\t%5lld\n",
-           proc_info.cmd, proc_info.resident,
-           proc_info.utime, proc_info.stime);
-  }
-  closeproc(proc);
+	PROCTAB* proc = openproc(PROC_FILLMEM | PROC_FILLSTAT | PROC_FILLSTATUS);
+	proc_t* proc_info;
+	while((proc_info = readproc(proc, NULL)) != NULL) {
+		printf("%20s:\t%5ld\t%5lld\t%5lld\n",
+				proc_info->cmd, proc_info->resident,
+				proc_info->utime, proc_info->stime);
+	}
+	closeproc(proc);
+	return 0;
 }
