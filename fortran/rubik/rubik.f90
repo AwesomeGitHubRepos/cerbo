297e6e14d120bcfd0665429679484a325d48f45f
Mark Carter <alt.mcarter@gmail.com>
2017-10-25 13:42:07 +0100

    correctiions to rubik
    Now passes some basic tests on rotation

--- fortran/rubik/rubik.f90
+++ fortran/rubik/rubik.f90
@@ -3,6 +3,7 @@ module rubik
         implicit none
 integer*1 m(6, 3, 3) ! face, row, col
 integer     f, r, c
+character :: colours(6) = (/ "W", "G", "R", "B", "O", "Y" /)
 
 contains
 
@@ -13,13 +14,24 @@ subroutine init()
         !call prmat()
 end subroutine        
 
-subroutine prmat()
+subroutine prmat(coloured)
 ! print matrix
-!use globs
+logical, optional :: coloured
+
+logical :: coloured_
+coloured_ = .false.
+if(present(coloured)) coloured_ = coloured
+
 do r=1, 3
 do f=1, 6
 do c=1, 3
+
+if(coloured_) then
+write (*, "(a1, a)", advance="no") colours(m(f, r, c)), ' '
+else
 write (*, "(i1, a)", advance="no") m(f, r, c), ' '
+end if
+
 enddo
 write (*, "(a2)", advance="no") '| '
 enddo
@@ -80,10 +92,10 @@ subroutine rot2()
         ! rotate about the 
         integer*1 tmp1(3)
         ! edges
-        tmp1 = m(5, 1:3, 1)
-        m(5, 1:3, 1) = m(1, 3:1:-1, 1)
+        tmp1 = m(5, 3:1:-1, 3)
+        m(5, 1:3, 3) = m(1, 3:1:-1, 1)
         m(1, 1:3, 1) = m(3, 1:3, 1)
-        m(3, 1:3, 1) = m(6, 3:1:-1, 1)
+        m(3, 1:3, 1) = m(6, 1:3, 1)
         m(6, 1:3, 1) = tmp1
 end subroutine
 
@@ -95,7 +107,7 @@ subroutine rot3()
         tmp1 = m(1, 3, 3:1:-1)
         m(1, 3, 1:3) = m(4, 1:3, 1)
         m(4, 1:3, 1) = m(6, 1, 3:1:-1)
-        m(6, 1, 1:3) = m(2, 3, 1:3)
+        m(6, 1, 1:3) = m(2, 1:3, 3)
         m(2, 1:3, 3) = tmp1
 end subroutine
 
@@ -105,8 +117,8 @@ subroutine rot4()
         ! edges
         tmp1 = m(3, 1:3, 3)
         m(3, 1:3, 3) = m(1, 1:3, 3)
-        m(1, 1:3, 3) = m(5, 1:3, 1)
-        m(5, 1:3, 1) = m(6, 1:3, 3)
+        m(1, 1:3, 3) = m(5, 3:1:-1, 1)
+        m(5, 1:3, 1) = m(6, 3:1:-1, 3)
         m(6, 1:3, 3) = tmp1
 end subroutine
 
@@ -134,7 +146,7 @@ end subroutine
 
 
 subroutine test_rotn(face)
-        integer face
+        integer :: face
        logical sane
 
        if(.true.) then
@@ -156,27 +168,50 @@ endif
         write(*, *) 'sane: ', sane
 end subroutine
 
-end module rubik
-
-program cube
-        use rubik
-
-        ! layout :
-        !      5
-        !   2  1 4
-        !      3
-        !      6
-
-!integer f ! face [1..6]
-!common /globs/ m
-
-call init()
-
+subroutine test_basic_rot()
 call test_rotn(1)
 call test_rotn(2)
 call test_rotn(3)
 call test_rotn(4)
 call test_rotn(5)
 call test_rotn(6)
+end subroutine
+
+subroutine apply_rotations(rots)
+        integer:: rots(:)
+        integer :: n, i
+        n = size(rots)
+        do i=1, n
+        call rotn(rots(i))
+        enddo
+end  subroutine
+
+end module rubik
+
+program cube
+        use rubik
+
+        ! layout :
+        !      5        O
+        !   2  1 4    G W B 
+        !      3        R
+        !      6        Y
+        ! 1 W white
+        ! 2 G green
+        ! 3 R red
+        ! 4 B blue
+        ! 5 O orange
+        ! 6 Y yellow
+
+!integer f ! face [1..6]
+!common /globs/ m
+
+call init()
+
+!call test_basic_rot()
+
+call apply_rotations((/1 ,2, 3, 4, 5, 6 /))
+call prmat(.true.)
+
 end program
 
