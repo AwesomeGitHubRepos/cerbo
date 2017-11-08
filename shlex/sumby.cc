7d48ebae9f0fb88946b73bd690238b00a32a85be
Mark Carter <alt.mcarter@gmail.com>
2017-11-08 21:00:12 +0000

    modified:   sumby.cc

--- shlex/sumby.cc
+++ shlex/sumby.cc
@@ -19,17 +19,25 @@ main()
 		if(it==rend) continue;
 		std::string key = *it++;
 		if(it==rend) continue;
-		double value = stod(*it);
+		//double value = stod(*it);
+		std::string vstr = *it;
+		double value = atof(vstr.c_str());
 
 		auto mit = m.find(key);
 		if(mit == m.end())
 			m[key] = value;
 		else
 			m[key] += value;
+
+		//std::cout << "sumby:" << key << "\t" << value << "\t" << m[key] << "\n";
+		//printf("sumby:%s\t%.3f\t%.3f\n", key.c_str(), value, m[key]);
 	}
 
-	for(auto mit=m.begin(); mit != m.end(); ++mit)
-		std::cout << mit->first << "\t" << mit->second << "\n";
+	for(auto mit=m.begin(); mit != m.end(); ++mit) {
+
+		printf("%s\t%.2f\n", mit->first.c_str(), mit->second);
+		//std::cout << mit->first << "\t" << mit->second << "\n";
+	}
 
 	return 0;
 }
