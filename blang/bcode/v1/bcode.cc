818ee41cf8b205866f247ed35f31bb6d24ac603e
Mark Carter <alt.mcarter@gmail.com>
2017-12-13 12:23:20 +0000

    Improved bcode compiler

--- blang/bcode/v1/bcode.cc
+++ blang/bcode/v1/bcode.cc
@@ -4,7 +4,6 @@
 #include <functional>
 #include <iomanip>
 #include <iostream>
-#include <map>
 #include <sstream>
 #include <stack>
 #include <string>
@@ -14,10 +13,69 @@
 using namespace std;
 
 typedef vector<uint8_t> bytes;
-typedef function<void()> vptr;
 
-stack<int64_t> stk; // the stack
+///////////////////////////////////////////////////////////////////////
+// the stack
 
+stack<int64_t> stk;
+
+int64_t pop_stack()
+{
+	int64_t v = stk.top();
+	stk.pop();
+	return v;
+}
+
+void push_stack(int64_t v)
+{
+	stk.push(v);
+}
+
+///////////////////////////////////////////////////////////////////////
+// An extensible collection of functions.
+// The idea is that you write more functions here that are relevant
+// to your application.
+
+void emit()
+{
+	int c = pop_stack();
+	putchar(c);
+}
+
+void hello()
+{
+	puts("hello world");
+}
+
+
+typedef struct { 
+	string  name; 
+	function<void()> fn; 
+} func_desc;
+
+
+vector<func_desc> vecfns = {
+	{"emit", emit},
+	{"hell", hello}
+};
+
+
+
+int find_vecfn(string name)
+{
+	for(int i = 0 ; i<vecfns.size(); ++i)
+		if(vecfns[i].name == name)
+			return i;
+
+	cerr << "find_vecfn:unknown function:" << name << "\n";
+	exit(1);
+}
+
+///////////////////////////////////////////////////////////////////////
+// Convenience and utility functions
+
+
+	
 template< typename T >
 std::string int_to_hex( T i )
 {
@@ -27,6 +85,9 @@ std::string int_to_hex( T i )
 		<< hex << i;
 	return stream.str();
 }
+
+
+///////////////////////////////////////////////////////////////////////
 
 void pushchar(bytes& bs, char c)
 {
@@ -44,106 +105,36 @@ void push64(bytes& bs, T  v)
 		bs.push_back(b[i]);
 }
 
-int64_t pop_stack()
-{
-	int64_t v = stk.top();
-	stk.pop();
-	return v;
-}
-
-void push_stack(int64_t v)
-{
-	stk.push(v);
-}
-
-void emit()
-{
-	int c = pop_stack();
-	putchar(c);
-}
-
-void hello()
-{
-	puts("hello world");
-}
-
-map<string, vptr>  functions = {
-	{"emit", emit},
-	{"hell", hello}
-};
-
-
-typedef struct { string  name; function<void()> fn; } func_desc;
-vector<func_desc> vecfns = {
-	{"emit", emit},
-	{"hell", hello}
-};
-
-
-vptr find_function(string name)
-{
-	auto it = functions.find(name);
-	if(it != functions.end())
-		return it->second;
-	else
-		return nullptr;
-
-}
-
-int find_vecfn(string name)
-{
-	for(int i = 0 ; i<vecfns.size(); ++i)
-		if(vecfns[i].name == name)
-			return i;
-
-	cerr << "find_vecfn:unknown function:" << name << "\n";
-	exit(1);
-}
-
-void test3()
-{
-	cout <<"test3\n";
-	auto fn = vecfns[find_vecfn("hell")].fn;
-	fn();
-}
 
 int main()
 {
 
-	cout << "Test1: Should print a !\n";
-	push_stack(33); // ASCII !
-	emit(); // works
-	cout << "\nTest1 finished\n"; 
+	// read program
+	stringstream sstr;
+	sstr << cin.rdbuf();
+	string prog = sstr.str();
 
-	cout << "Test 2: s/b same result\n"; // works
-	{
-		push_stack(33);
-		auto fn = find_function("emit");
-		fn();
-		cout << "emit:" << &fn << "\n";
-	}
-	cout << "\nTest 2 finisned\n";
-
-	test3();
-	//exit(0);
-
-	string prog("p072 xemit p073 xemit p010 xemit xhell 0");
 	
-	//prog = "xhell 0";
-
 	// compile
 	bytes bcode;
 	for(int i = 0 ; i < prog.size(); ++i) {
 		char c = prog[i];
 		switch(c) {
-			case ' ': break; // ignore spaces
+			case ' ': // ignore white space
+			case '\r':
+			case '\t':
+			case '\n':
+				break; 
+			case '#': // ignore comments
+				while(prog[++i] != '\n');
+				break;
 			case '0' : 
 				  pushchar(bcode, '0'); 
 				  break;
 			case 'p' :{
        					  pushchar(bcode, 'p');
 					  auto val = (prog[++i] -'0') * 100 + (prog[++i]-'0')*10 +(prog[ ++i]- '0');
-					  cout << "compiling p:" << val << "\n";
+					  //cout << "compiling p:" << val << "\n";
        					  push64(bcode, val);
 				  }
 				  break;				   
@@ -151,17 +142,6 @@ int main()
        					  pushchar(bcode, 'x');
        					  string function_name = { prog[++i],  prog[++i], prog[++i], prog[++i]};
 					  pushchar(bcode, find_vecfn(function_name)); 
-					  /*
-					  cout << "function name:" << function_name << ".\n";
-					  auto function_ptr = find_function(function_name);
-					  int64_t ptr = reinterpret_cast<int64_t>(&function_ptr);
-					  cout << "x function pointer: " << int_to_hex(ptr) << "\n";
-					  if(function_ptr)
-						  push64(bcode, ptr);
-					  else {
-						  cerr << "Could not find function:" << function_name << ".\n";
-						  exit(1);
-					  }*/
 					  break;
 				  }
 			default:
@@ -199,14 +179,6 @@ int main()
 					  auto fn_idx = bcode[++pc];
 					  auto fn = vecfns[fn_idx].fn;
 					  fn();
-					  /*
-					  uint8_t b[8];
-					  int64_t addr = 0 ; // (int64_t) b;
-					  for(uint8_t i = 0 ; i <8; ++i) b[i] = bcode[++pc];
-					  memcpy(&addr, b, 8);
-					  cout << "x function pointer: " << int_to_hex(addr) << "\n";
-					  reinterpret_cast< void(*)() > (addr) ();
-					  */
 					  pc++;
 
 				  }
---
+++ blang/bcode/v1/hi.asm
@@ -0,0 +1,8 @@
+# print HI to screen
+p072  # ascii H
+xemit # print the char to screen
+p073  # ascci I
+xemit
+p010  # \n
+xemit
+0     # halt program
