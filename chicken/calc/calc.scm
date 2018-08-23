4de430e459a427d5c381fcf3a6cadfc9c35daa92
Mark Carter <alt.mcarter@gmail.com>
2018-08-23 19:07:01 +0100

    Made chicken calc compilable

--- chicken/calc/calc.scm
+++ chicken/calc/calc.scm
@@ -1,13 +1,21 @@
 ;;(load "calc.scm")
 (require-extension lalr-driver)
-(include "calc.yy.scm")
+
+(cond-expand
+ (compiling (declare (uses calc-yy)))
+ (else (load "calc-yy.scm")))
+;;(import calc.yy)
 
 (define (my-make-token sym val)
   (make-lexical-token sym 0 val))
 
-(include "lex-out.scm")
+
+(cond-expand
+ (compiling (declare (uses lex-out)))
+ (else (include "lex-out.scm")))
 
 (lexer-init 'port (open-input-file "input.txt"))
 ;;(lexer-init 'string "14+12+5")
 
 (print (calc-parser lexer print))
+
