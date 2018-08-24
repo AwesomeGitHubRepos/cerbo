f427ced3a7df44af220975e0628bd2930fea6405
Mark Carter <alt.mcarter@gmail.com>
2018-08-24 08:13:05 +0100

    chicken blang improvements
    It's looking really good now.

--- blang/chicken/blangc.scm
+++ blang/chicken/blangc.scm
@@ -2,22 +2,22 @@
 (require-extension lalr-driver)
 
 (cond-expand
- (compiling (declare (uses calc-yy)))
- (else (load "calc-yy.scm")))
-;;(import calc.yy)
+ (compiling (declare (uses grammar-out)))
+ (else (load "grammar-out.scm")))
+
 
 (define (my-make-token sym val)
   (make-lexical-token sym 0 val))
 (define (simple-token sym)
-  (my-make-token sym 0 sym))
+  (my-make-token sym sym))
 
 
 (cond-expand
  (compiling (declare (uses lex-out)))
  (else (include "lex-out.scm")))
 
-(lexer-init 'port (open-input-file "input.txt"))
-;;(lexer-init 'string "14+12+5")
+;;(lexer-init 'port (open-input-file "input.txt"))
+(lexer-init 'string "14+12+5")
 
-(print (calc-parser lexer print))
+(print (eval (blang-parser lexer print)))
 
