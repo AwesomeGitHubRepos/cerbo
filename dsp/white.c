d248323fba3a6f4ee5e2ae3aadc079fcc6c7adbe
Mark Carter <alt.mcarter@gmail.com>
2019-07-06 14:36:37 +0100

    modified:   ../../dsp/white.c

--- dsp/white.c
+++ dsp/white.c
@@ -10,14 +10,15 @@ int main()
 	FILE *fp = fopen("white.raw", "wb");
 	assert(fp);
 	//double PI = 3.14159265;
-	double sample_rate = 8000; // in Hz
-	double freq = 440; // in  Hz
+	//double sample_rate = 8000; // in Hz
+	//double freq = 440; // in  Hz
 	srand(time(NULL));
 	int sample_secs = 10; // produce 10 seconds worth of data
-	for(int t=0;t<sample_secs * sample_rate; ++t) {		
+	//for(int t=0;t<sample_secs * sample_rate; ++t) {		
+	for(int t=0;t<131072; ++t) {		
 		uint8_t pcm = rand() % 256;
 		fwrite(&pcm, 1, sizeof(pcm), fp);
-
+		printf("%d\n", pcm);
 	}
 	fclose(fp);
 }
