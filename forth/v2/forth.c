319a4c05d01deaddd5d4c3a1c7efd1cd46b1e55f
Mark Carter <alt.mcarter@gmail.com>
2019-11-14 12:21:30 +0000

    forth working a little better

--- forth/v2/forth.c
+++ forth/v2/forth.c
@@ -1,8 +1,62 @@
+/*
+ * I release this file into the Public Domain, together with
+ * all the other files in this directory.
+ *
+ * Enjoy!
+ *
+ * Mark Carter, Nov 2019
+ *
+ */
+
+#include <assert.h>
+#include <stdbool.h>
+#include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 
-typedef intptr_t cell_t
+typedef intptr_t cell_t;
 
+cell_t heap[10000];
+cell_t* hptr = heap;
+
+bool g_compiling = false;
+bool g_show_prompt = true;
+
+//char _TIB[136]; // The input buffer
+//char* TIB = _TIB;
+char tib[132];
+int g_bytes_read = 0; // number of bytes read into TIB
+
+
+
+void process_tib();
+
+
+
+
+void add_primitives()
+{
+	// TODO
+}
+
+void add_derived()
+{
+	// TODO
+}
+
+void process_tib()
+{
+	// TODO
+}
 int main()
 {
+	assert(sizeof(size_t) == sizeof(cell_t));
+	g_compiling = false;
+	add_primitives();
+	add_derived();
+	while(fgets(tib, sizeof(tib), stdin)) {
+		process_tib();
+		if(g_show_prompt) puts("  ok");
+	}
 	return 0;
 }
