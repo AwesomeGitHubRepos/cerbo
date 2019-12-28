ceb9ede72147e4608d96b3e7298a7ead336d8acf
Mark Carter <alt.mcarter@gmail.com>
2019-12-28 17:32:38 +0000

    added catasm.s

---
+++ arm/hello/catasm.s
@@ -0,0 +1,21 @@
+@ `cat' in assembler
+
+.global main
+main:
+	@ prologue
+    	push    {ip, lr}
+
+
+loop:
+	bl	getchar
+	cmp	r0, #-1 @ test for EOF
+	beq	end_loop
+	blne	putchar
+	b	loop
+end_loop:
+
+
+	@ Exit from 'main'. This is like 'return 0' in C.
+    	mov     r0, #0    @ Return 0.
+    	pop     {ip, pc} 
+
