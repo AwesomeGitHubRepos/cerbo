6006a5648a4caddd33ae2e1d31bcfc2f3decb877
Mark Carter <alt.mcarter@gmail.com>
2020-01-11 14:42:00 +0000

    modified:   mean.s

--- arm/hello/mean.s
+++ arm/hello/mean.s
@@ -6,9 +6,29 @@ main:
 	@ prologue
     	push    {ip, lr}
 
-	@ldr	r1, addr_values
+	@ set up counter
+	ldr	r1, =one
+	vldr	s8, [r1] @ contains the value 1.0
+	vsub.f32 s7, s7, s7 @ count
+
 	ldr	r1, =values
+	ldr	r4, =string
+	vsub.f32 s0, s0, s0
+
+	@mov	r6, #1 
+	@vmov	s6, r6 @ increment
+	@vmov.f32 s6, #1	
+loop:
+	@add	r6, r6, #1
+	vadd.f32 s7, s7, s8
 	vldr	s14, [r1]
+	vadd.f32 s0, s0, s14
+	add	r1, r1, #4
+	cmp	r1, r4
+	bne	loop
+
+	vdiv.f32 s0, s0, s7
+
 	bl 	print
 
 
@@ -18,7 +38,7 @@ main:
 
 print:
 	push {lr}
-	vcvt.f64.f32 d5, s14
+	vcvt.f64.f32 d5, s0
 	ldr r0, =string
 	vmov r1, r2, d5
 	bl printf
@@ -29,5 +49,6 @@ addr_values:
 
 .data
 
+one: .float 1.0 @ don't change this
 values: .float 3.8, -1.4, -0.1, 0.7, -0.2
 string: .asciz "Average is: %f\n"
