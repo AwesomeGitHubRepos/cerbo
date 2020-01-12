fac1501f9d9522f01d2fb5e910d5c65ff0688920
Mark Carter <alt.mcarter@gmail.com>
2020-01-12 22:23:16 +0000

    more fun with forth fin

---
+++ forth/fin/fin.c
@@ -0,0 +1,103 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+typedef long int int64;
+typedef void (*codeptr)();
+
+
+typedef struct {int64 hp; bool prim; } dict_s;
+dict_s dict[256];
+char lwc; // last work created
+char heap[1000];
+int hptr = 0;
+
+void define()
+{
+	int ch = getchar();
+	lwc = ch;
+	dict[ch].hp = hptr;
+	dict[ch].prim = false;
+
+	while(ch = getchar()) {
+		heap[hptr++] = ch;
+		if(ch== ';') break;
+	}
+	puts("Finished definition");
+
+}
+
+void run()
+{
+	puts("Running");
+	char ch = lwc;
+	while(ch != ';') {
+		dict_s d = dict[ch];
+		if(d.prim) {
+			codeptr fn = (codeptr) d.hp;
+			fn();
+		}
+		// TODO
+	}
+	puts("Finished running");
+
+
+
+}
+
+
+void p_key()
+{
+	puts("key called");
+}
+
+
+void p_emit()
+{
+	puts("emit called");
+}
+
+typedef struct {char name; void* fn; } prim_s;
+prim_s prims[] = {
+	{'k', p_key},
+	{'e', p_emit},
+	0
+};
+
+void add_prims()
+{
+	prim_s* p = prims;
+	while(p) {
+		char ch = p->name;
+		dict[ch].hp = (int64) p->fn;
+		dict[ch].prim = true;
+		p++;
+	}
+
+}
+
+int main()
+{
+	printf("int:%ld, void*:%ld\n", sizeof(int64), sizeof(void*));
+	assert(sizeof(int64) >= sizeof(void*));
+	int ch;
+	while(ch = getchar()) {
+		if(ch == EOF) break;
+		switch(ch) {
+			case ':':
+				define();
+				break;
+			case 'r':
+				run();
+				break;
+			case 'h': // halt
+				goto finis;
+				break;
+		}
+		puts("ok");
+	}
+finis:
+	puts("Halted");
+
+	return 0;
+}
