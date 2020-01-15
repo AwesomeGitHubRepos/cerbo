201288ac303ccf4ed578aca7b1d52814d55a1ec1
Mark Carter <alt.mcarter@gmail.com>
2020-01-15 10:01:13 +0000

    new file:   closures.nim

---
+++ nim/closures.nim
@@ -0,0 +1,15 @@
+#  vim:  ts=4 sw=4 softtabstop=0 expandtab shiftwidth=4 smarttab syntax=off
+
+proc ioto(): int =
+    var i = 0
+    proc fn (): int =
+        i = i + 1
+        return i
+    result = fn
+
+
+var j = iota()
+echo j() , j()
+var k = iota()
+echo k(), k(), k()
+echo j()
