bfcac88ad861b09c3e4f12a55df589c092d9645a
Mark Carter <alt.mcarter@gmail.com>
2020-01-21 16:33:10 +0000

    New meta.nim

---
+++ schorre/nim/meta.nim
@@ -0,0 +1,18 @@
+#  vim:  ts=4 sw=4 softtabstop=0 expandtab shiftwidth=4 smarttab syntax=off
+
+proc found_instr(line:string) = 
+    return
+
+proc found_label(line:string) =
+    echo "found label:", line, "."
+
+proc top() =
+    for line in lines "schorre-metaii.vm":
+        if len(line) == 0: continue
+        if line[0] == '\t' or line[0] == ' ':
+            found_instr(line)
+        else:
+            found_label(line)
+
+top()        
+
