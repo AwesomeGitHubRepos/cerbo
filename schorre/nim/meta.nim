2723be8db7626be8e4477e4a8111f28423adfe7b
Mark Carter <alt.mcarter@gmail.com>
2020-01-21 17:54:31 +0000

    modified:   meta.nim

--- schorre/nim/meta.nim
+++ schorre/nim/meta.nim
@@ -1,20 +1,32 @@
 #  vim:  ts=4 sw=4 softtabstop=0 expandtab shiftwidth=4 smarttab syntax=off
 
+import re
+import strutils
+import system
 import tables
 
-var opcodes: seq[int]
+type
+    opcode_t =tuple[arg1, arg2:string]
+
+var opcodes: seq[seq[string]]
 
 var labels = initTable[string, int]()
 
 proc found_label(line:string) =
     labels[line] = len(opcodes)
-    echo "found label:", line, "."
 
 proc found_instr(line:string) = 
-    return
+    let args = line.split(re"\s+", 2)
+    if len(args) == 2:
+        opcodes.add(@[args[1]])
+    else:
+        var arg:string = args[2]
+        if arg[0] == '\'':
+            arg = arg.substr(1, len(arg)-2 )
+        opcodes.add(@[args[1], arg])
 
 
-proc top() =
+proc parse() =
     for line in lines "meta.asm":
         if len(line) == 0: continue
         if line[0] == '\t' or line[0] == ' ':
@@ -22,5 +34,7 @@ proc top() =
         else:
             found_label(line)
 
-top()        
+parse()        
 
+
+echo opcodes
