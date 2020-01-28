c1221c36b6d24f4a946557e6978396e985197ee6
Mark Carter <alt.mcarter@gmail.com>
2020-01-28 22:28:19 +0000

    modified:   ../racket/hard-coded.rkt

--- schorre/racket/hard-coded.rkt
+++ schorre/racket/hard-coded.rkt
@@ -26,6 +26,9 @@ PROGRAM = '.SYNTAX' .ID $ ST .END .,
 (define (l-or-d? c) (or (digit? c) (letter? c)))
 (define (white? c) (member c '(#\tab #\space #\newline)))
 (define (non-white? c) (and (not (white? c)) (not (eof-object? c))))
+(define (dotty? c)
+  (and (non-white? c)
+       (not (char=? #\( c))))
 
 (define (accum pred)
   (list->string
@@ -51,6 +54,7 @@ PROGRAM = '.SYNTAX' .ID $ ST .END .,
   ;;(display (pc))
   (define res (cond
     [(eof-object? (pc)) (list 'eof (rc))]
+    [(char=? #\. (pc)) (list 'special (accum dotty?))]
     [ (char=? #\' (pc)) (list 'str (get-string))]
     [(digit? (pc)) (list 'number (accum digit?))]
     [(letter? (pc)) (list 'id (accum non-white?))]
@@ -70,71 +74,122 @@ PROGRAM = '.SYNTAX' .ID $ ST .END .,
   (set! yytype (car yy))
   (set! yytext (cadr yy)))
 
-(define (out lst)
-  (unless (empty? lst)
-    (for-each (lambda (x)
-                (cond
-                  [(eq? x '*) (display yytext)]
-                  [else (display x)])
-                #t)
-              lst)
-    (newline)))
+
+(define ** '**)
+(define mtext #f) ; matched text
+(define (match!) (set! mtext yytext))
+
+(define (matcher yes) (if yes (begin (match!) (yylex) #t) #f))
+(define (expect str)  (yytext=? str))
+(define (.number)  (yytype? 'number))
+(define (.id) (yytype? 'id))
 
 
-(define (expect str)
-  ;;(printf "expect called with ~s\n" str)
-  (if (yytext=? str)
-      (begin
-        (yylex)
-        #t)
-      #f))
+;;(define (expect str) (if (yytext=? str) (begin (match!) (yylex) #t) #f))
 
 
 
-(define (number)
-  (if (yytype? 'number)
-      (begin
-        (printf "LD ~a\n" yytext)
-        (yylex)
-        #t)
-      #f))
+
 
 (define (.yytext txt) (printf "yytext in ~a = ~a\n" txt yytext) #t)
+;;(define (hopes x) (if (yytext=? x) (begin (yylex) #t) #f))
 
-(define (bracket)
- 
-  (and (expect "(")  (ex1) (expect ")") ))
 
-(define (ex3)
-  ;;(.yytext "ex3")
-  (or (number) (bracket)))
+
   
-(define (ex2)
-  ;;(.yytext "ex2")
-  (ex3)
-  (while (yytext=? "*")
-    (yylex)
-    (ex3)
-    (displayln "MLT"))
-  #t)
-
-(define (ex1)
-  ;;(.yytext "ex1a")
-  (ex2)
-  (while (yytext=? "+")
-         ;;(.yytext "ex1b")
-         (yylex)
-         (ex2)
-         (displayln "ADD"))
-  #t)
+(define (out . args)
+  (lambda ()
+    (each arg args
+          (if (eq? arg '**)
+              (display mtext)
+              (display arg))
+          (display " "))
+    ;;(apply display args)
+    ;;(newline)
+    #t))
 
 
-(define (do-it user-data)
+
+(define-syntax puff
+  (syntax-rules ()
+    ((_ expr)
+     (cond
+       [(string? expr) (expect expr)]
+       [(procedure? expr) (expr)]
+       [else expr]))
+    ((_ expr others ...)
+     (begin
+       (puff expr)
+       (puff others ...)))))
+
+(define-syntax-rule ($ test body ...)
+  (begin
+    (while (puff test)
+           (and (puff body ...)))
+    #t))
+
+(define (do-it init user-data)
   (pin! user-data)
   (yylex)
-  (ex1)
+  (init)
   #t)
 
-(do-it "( 22+23)  * ( 24 + 25 )")
-;;(do-it "22  + 23")
+
+(define-syntax-rule (pout target  outputs ...)
+  (let ()
+    (define ok
+      (cond
+        [(string? target) (yytext=? target)]
+        [(procedure? target) (target)]
+        [else (raise "pout is confused")]))
+    (when ok
+      (match!)
+      ((out  outputs ...))
+      (yylex))
+    ok))
+
+
+
+(define (bracket) (and (expect "(")  (ex1) (expect ")") ))
+(define (ex3) (or (pout .number "LD" **) (bracket)))
+(define (ex2) (puff ex3 ($ "*" (pout ex3 "MLT" "\n")) #t))
+(define (ex1) (puff ex2 ($ "+" (pout ex2 "ADD" "\n")) #t))
+
+
+
+
+;;(do-it ex1 "( 22+23+30)  * ( 24 + 25 )")
+
+
+
+
+
+(define (M.EX1)  
+  (pout .id "(pout " ** ")"))
+
+
+(define (M.ST)
+  (pout .id "(define (" ** ")")
+ 
+  (pout "=")
+  ;; (.yytext "M.ST")
+  (M.EX1)
+  (pout ".," ") statement ended\n"))
+
+(define (0+ pat)
+  (if (pat)
+      (0+ pat)
+      #t))
+
+(define (M.PROG)
+  (pout ".SYNTAX"  "YES-SYN" ) (pout .id  "PROGRAM NAME IS " ** "\n")
+  ;;(while (M.ST) #t)
+  (0+ M.ST)
+  (pout ".END" "\nAnd we're done\n"))
+  
+(do-it M.PROG ".SYNTAX PROGRAM
+
+PROGRAM  =  ST .,
+ST = BAZ .,
+.END")
 
