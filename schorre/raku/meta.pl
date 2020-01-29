697525488ce4d1f9aff7422a96c69094b88b3c6b
Mark Carter <alt.mcarter@gmail.com>
2020-01-29 12:54:29 +0000

    modified:   meta.pl

--- schorre/raku/meta.pl
+++ schorre/raku/meta.pl
@@ -1,5 +1,59 @@
- sub isalpha($x) { return $x (elem) (['a'..'z'] (|) ['A'..'Z']) ; }
- sub isdigit($x) { return $x (elem) ['0'..'9']; }
+sub isalpha($x) { return $x (elem) (['a'..'z'] (|) ['A'..'Z']) ; }
+sub isdigit($x) { return $x (elem) ['0'..'9']; }
+sub isalnum($x) { return isalpha($x) or isdigit($x); }
+sub iswhite($x) { return " \t\rn\n".contains($x); }
 
- say isalpha('q');
- say isalpha('0');
+my $text;
+my $pos;
+sub cpeek() { return ~$text[$pos]; }
+sub cget()  { return ~$text[$pos++]; }
+sub init-parser($str)
+{
+	$text = $str;
+	$pos = 0;
+}
+
+my $yytype;
+my $yytext;
+
+sub collect() { $yytext ~=  cget(); }
+
+sub yylex()
+{
+	$yytype = "unk";
+	$yytext = "";
+	while iswhite(cpeek()) { cget(); }
+	given cpeek() {
+		when isdigit(cpeek()) {
+			$yytype = "num";
+			while isdigit(cpeek()) { collect(); }
+		}
+		when (cpeek() eq "'") { # 39 is '
+			$yytype = "str";
+			cget();
+			while cpeek() != "'" { collect(); }
+			cget();
+		}
+		when cpeek() eq "." {
+			$yytype = "str";
+			collect();
+			while isalnum(cpeek()) or cpeek() == "," { collect(); }
+		}
+		when isalpha(cpeek()) {
+			$yytype = "id";
+			while isalnum(cpeek()) { collect(); }
+		}
+		default {
+			$yytype = "str";
+			collect();
+		}
+	}
+}
+
+sub debug-lexer()
+{
+	while yylex() { say "$yytype <$yytext>"; }
+}
+
+init-parser "hello world";
+debug-lexer();
