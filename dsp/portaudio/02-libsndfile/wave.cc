628d891d81a9f18baf09aa4a10872e1363ca48e4
Mark Carter <alt.mcarter@gmail.com>
2021-06-23 10:02:35 +0100

    tweaks to wave file

--- dsp/portaudio/02-libsndfile/wave.cc
+++ dsp/portaudio/02-libsndfile/wave.cc
@@ -1,9 +1,17 @@
+#include <iostream>
 #include <sndfile.h>
+
+using namespace std;
 
 int main()
 {
 	SF_INFO sfinfo;
+	sfinfo.format = 0;
 	SNDFILE* sndfile = sf_open("/home/pi/Music/sheep.wav", SFM_READ, &sfinfo);
+	cout << "Sample rate: " << sfinfo.samplerate << "\n";
+	cout << "Channels: " << sfinfo.channels << "\n";
+	printf("Format: 0x%X, ", sfinfo.format);
+	cout << "Wave file?: " << ((SF_FORMAT_WAV>>2) == (sfinfo.format>>2)) << "\n";
 
 	sf_close(sndfile);
 	return 0;
