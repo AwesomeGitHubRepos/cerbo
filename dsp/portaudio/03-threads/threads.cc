ca3377559a761594abe259261ec5948569436a7a
Mark Carter <alt.mcarter@gmail.com>
2021-06-23 11:06:12 +0100

    completed threads project
    Surprisingly easy

--- dsp/portaudio/03-threads/threads.cc
+++ dsp/portaudio/03-threads/threads.cc
@@ -1,35 +1,35 @@
 #include <assert.h>
 #include <iostream>
-#include <sndfile.h>
+//#include <sndfile.h>
+#include <thread>
+#include <semaphore.h>
+#include <unistd.h>
+
+
+
+static sem_t sem;
 
 using namespace std;
 
+void worker()
+{
+	static int i = 0;
+	while(1) {
+		printf("Worker %d\n", i++);
+		sem_wait(&sem);
+	}
+}
+
 int main()
 {
-	SF_INFO sfinfo;
-	sfinfo.format = 0;
-	SNDFILE* sndfile = sf_open("/home/pi/Music/sheep.wav", SFM_READ, &sfinfo);
-	assert(sndfile);
-	cout << "Sample rate: " << sfinfo.samplerate << "\n";
-	int nchannels = sfinfo.channels;
-	cout << "Channels: " << nchannels << "\n";
-	printf("Format: 0x%X, ", sfinfo.format);
-	cout << "Wave file?: " << ((SF_FORMAT_WAV>>2) == (sfinfo.format>>2)) << "\n";
-	printf("Sizeof short: %d\n", sizeof(short));
+	thread th(worker);
+	sem_init(&sem, 0, 0);
 
-	FILE* fp = fopen("out.raw", "w");
-	assert(fp);
-	const int NSAMPLES = 512;
-	short buf[NSAMPLES*nchannels];
-	while(sf_count_t nread = sf_readf_short(sndfile, buf, NSAMPLES)) {
-		for(int i = 0; i < nread; ++i) {
-			// output only the first channel
-			fwrite(buf + 2* i, 1, sizeof(short), fp);
-		}
+	while(1) {
+		sleep(2);
+		sem_post(&sem);
 	}
 
-
-	fclose(fp);
-	sf_close(sndfile);
+	th.join();
 	return 0;
 }
