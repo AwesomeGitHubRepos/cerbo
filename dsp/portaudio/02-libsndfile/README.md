8534e9ccfe11a53bc7406fbe43099db22b6b7ab7
Mark Carter <alt.mcarter@gmail.com>
2021-06-23 10:32:45 +0100

    wave fiel reading now working

--- dsp/portaudio/02-libsndfile/README.md
+++ dsp/portaudio/02-libsndfile/README.md
@@ -3,3 +3,11 @@
 Example demo of reading a wave file.
 
 
+To play (or similar):
+```
+aplay -f S16_LE -r 44100 out.raw
+```
+
+## Status
+
+2021-06-23 Started. Working.
--- dsp/portaudio/02-libsndfile/wave.cc
+++ dsp/portaudio/02-libsndfile/wave.cc
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <iostream>
 #include <sndfile.h>
 
@@ -8,11 +9,27 @@ int main()
 	SF_INFO sfinfo;
 	sfinfo.format = 0;
 	SNDFILE* sndfile = sf_open("/home/pi/Music/sheep.wav", SFM_READ, &sfinfo);
+	assert(sndfile);
 	cout << "Sample rate: " << sfinfo.samplerate << "\n";
-	cout << "Channels: " << sfinfo.channels << "\n";
+	int nchannels = sfinfo.channels;
+	cout << "Channels: " << nchannels << "\n";
 	printf("Format: 0x%X, ", sfinfo.format);
 	cout << "Wave file?: " << ((SF_FORMAT_WAV>>2) == (sfinfo.format>>2)) << "\n";
+	printf("Sizeof short: %d\n", sizeof(short));
 
+	FILE* fp = fopen("out.raw", "w");
+	assert(fp);
+	const int NSAMPLES = 512;
+	short buf[NSAMPLES*nchannels];
+	while(sf_count_t nread = sf_readf_short(sndfile, buf, NSAMPLES)) {
+		for(int i = 0; i < nread; ++i) {
+			// output only the first channel
+			fwrite(buf + 2* i, 1, sizeof(short), fp);
+		}
+	}
+
+
+	fclose(fp);
 	sf_close(sndfile);
 	return 0;
 }
