60caa1ebf71087a89b936076baecb03e667117cd
Mark Carter <alt.mcarter@gmail.com>
2021-06-23 13:34:17 +0100

    tweaks

--- dsp/portaudio/04-playwav/playwav.cc
+++ dsp/portaudio/04-playwav/playwav.cc
@@ -12,10 +12,12 @@ static sem_t sem;
 
 using namespace std;
 
+#define FPB 512 // frames per buffer
+
 SNDFILE* sndfile = nullptr;
 
 
-void worker()
+void reader()
 {
 	static int i = 0;
 	while(1) {
@@ -24,9 +26,18 @@ void worker()
 	}
 }
 
+int callback(const void* ibuffer, void *obuffer, unsigned long fpb, 
+		const PaStreamCallbackTimeInfo *timeInfo, PaStreamCallbackFlags statusFlags,
+		void* userData)
+{
+	assert(obuffer != NULL);
+
+	return paContinue;
+}
+
 int main()
 {
-	thread th(worker);
+	thread th(reader);
 	sem_init(&sem, 0, 0);
 
 	// open soundfile
@@ -41,9 +52,22 @@ int main()
 	cout << "Wave file?: " << ((SF_FORMAT_WAV>>2) == (sfinfo.format>>2)) << "\n";
 	printf("Sizeof short: %d\n", sizeof(short));
 
+	// init sound stream
 	PaError paerr;
 	paerr = Pa_Initialize();
 	assert(paerr == paNoError);
+	PaStream* strm;
+	paerr = Pa_OpenDefaultStream(&strm, 
+			0, // number input channels
+			1, // number output channels
+			paInt16, // sample format: signed 16 bit format
+			44100, // sample rate
+			FPB, // frames per buffer
+			callback,
+			NULL);
+	assert(paerr == paNoError);
+
+
 
 	while(1) {
 		sleep(2);
