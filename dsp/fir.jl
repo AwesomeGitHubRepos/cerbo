b3e88fc2c8a31616bb6da9a506a014da1d6c159e
Mark Carter <alt.mcarter@gmail.com>
2021-07-13 21:25:08 +0100

    new file:   fir.jl

---
+++ dsp/fir.jl
@@ -0,0 +1,19 @@
+# include("fir.jl")
+# import Pkg; Pkg.add("DSP")
+import DSP
+
+a1 = read(expanduser("~/Music/sheep-16k.raw"))
+a2 = Array{Float64}(a1)
+
+h = ones(20)
+for i = 1:length(h)
+	h[i] = i
+end
+# h = ones(5)
+h = h/sum(h)
+h = reverse(h)
+
+a3 = DSP.conv(a2, h)
+a4 = round.(UInt8, a3)
+write("out.bin", a4)
+
